f77b57980948a1e998c8d7ccc038c46e53aa68ca
akpobasa12345 <victorakpobasa2020@gmail.com>
2024-11-17 15:19:21 +0100

    fix questionjs

--- questions.js
+++ questions.js
@@ -1,30 +1,13 @@
 let frontendQuiz = [
   {
     numb: 1,
-    question: "What is the default position value in CSS?",
-    answer: "D. static",
-    options: ["A. relative", "B. absolute", "C. fixed", "D. static"],
-  },
-  {
-    numb: 2,
-    question: "What is the purpose of the z-index property in CSS?",
-    answer: "B. To specify the stack order of elements",
-    options: [
-      "A. To change the zoom level of an element",
-      "B. To specify the stack order of elements",
-      "C. To define the position of an element",
-      "D. To set the opacity of an element",
-    ],
-  },
-  {
-    numb: 3,
     question:
       "Which HTML attribute is used to specify a unique identifier for an element?",
     answer: "B. id",
     options: ["A. class", "B. id", "C. name", "D. key"],
   },
   {
-    numb: 4,
+    numb: 2,
     question:
       "What does the this keyword refer to in JavaScript within a regular function?",
     answer: "D. The object that called the function",
@@ -36,7 +19,32 @@ let frontendQuiz = [
     ],
   },
   {
+    numb: 3,
+    question:
+      "Which pseudo-class is used to style an element when it is hovered over?",
+    answer: "A. :hover",
+    options: ["A. :hover", "B. :focus", "C. :active", "D. :checked"],
+  },
+  {
+    numb: 4,
+    question:
+      "Which JavaScript feature allows functions to remember the scope in which they were created?",
+    answer: "B. Closures",
+    options: ["A. Callbacks", "B. Closures", "C. Prototypes", "D. Promises"],
+  },
+  {
     numb: 5,
+    question: "What is the purpose of the z-index property in CSS?",
+    answer: "B. To specify the stack order of elements",
+    options: [
+      "A. To change the zoom level of an element",
+      "B. To specify the stack order of elements",
+      "C. To define the position of an element",
+      "D. To set the opacity of an element",
+    ],
+  },
+  {
+    numb: 6,
     question: "What does HTML stand for?",
     answer: "C. Hyper Text Markup Language",
     options: [
@@ -47,53 +55,7 @@ let frontendQuiz = [
     ],
   },
   {
-    numb: 6,
-    question: "Which method is used to stop event propagation in JavaScript?",
-    answer: "B. event.stopPropagation()",
-    options: [
-      "A. event.preventDefault()",
-      "B. event.stopPropagation()",
-      "C. event.stopDefault()",
-      "D. event.cancelBubble()",
-    ],
-  },
-  {
     numb: 7,
-    question: "What will the following code output? console.log(typeof NaN);",
-    answer: "A. number",
-    options: ["A. number", "B. NaN", "C. undefined", "D. object"],
-  },
-  {
-    numb: 8,
-    question:
-      "Which pseudo-class is used to style an element when it is hovered over?",
-    answer: "A. :hover",
-    options: ["A. :hover", "B. :focus", "C. :active", "D. :checked"],
-  },
-  {
-    numb: 9,
-    question: "What does the innerHTML property do in JavaScript?",
-    answer: "A. Gets or sets the content of an HTML element",
-    options: [
-      "A. Gets or sets the content of an HTML element",
-      "B. Adds a new HTML element",
-      "C. Executes JavaScript within an element",
-      "D. Removes an HTML element",
-    ],
-  },
-  {
-    numb: 10,
-    question: "What is the main purpose of the section tag in HTML5?",
-    answer: "B. To define sections within a document",
-    options: [
-      "A. To group header elements",
-      "B. To define sections within a document",
-      "C. To display a horizontal line",
-      "D. To create an interactive element",
-    ],
-  },
-  {
-    numb: 11,
     question:
       "How do you apply a CSS rule only when the screen width is less than 768px?",
     answer: "A. @media screen and (max-width: 768px)",
@@ -105,14 +67,24 @@ let frontendQuiz = [
     ],
   },
   {
-    numb: 12,
-    question:
-      "Which JavaScript array method is used to create a new array with elements that pass a test?",
-    answer: "A. filter()",
-    options: ["A. filter()", "B. map()", "C. reduce()", "D. forEach()"],
+    numb: 8,
+    question: "Which method is used to stop event propagation in JavaScript?",
+    answer: "B. event.stopPropagation()",
+    options: [
+      "A. event.preventDefault()",
+      "B. event.stopPropagation()",
+      "C. event.stopDefault()",
+      "D. event.cancelBubble()",
+    ],
   },
   {
-    numb: 13,
+    numb: 9,
+    question: "What will the following code output? console.log(typeof NaN);",
+    answer: "A. number",
+    options: ["A. number", "B. NaN", "C. undefined", "D. object"],
+  },
+  {
+    numb: 10,
     question: "What does the async attribute in a script tag do?",
     answer: "B. Loads the script while the HTML parsing continues",
     options: [
@@ -123,7 +95,7 @@ let frontendQuiz = [
     ],
   },
   {
-    numb: 14,
+    numb: 11,
     question: "What is the difference between == and === in JavaScript?",
     answer: "A. == checks only value, === checks value and type",
     options: [
@@ -134,14 +106,76 @@ let frontendQuiz = [
     ],
   },
   {
-    numb: 15,
-    question: "Which CSS property is used to make text bold?",
-    answer: "A. font-weight",
+    numb: 12,
+    question:
+      "Which JavaScript array method creates a new array with elements that pass a test?",
+    answer: "A. filter()",
+    options: ["A. filter()", "B. map()", "C. reduce()", "D. forEach()"],
+  },
+  {
+    numb: 13,
+    question: "Which CSS property is used to create a flex container?",
+    answer: "B. display: flex",
     options: [
-      "A. font-weight",
-      "B. text-style",
-      "C. font-style",
-      "D. text-decoration",
+      "A. flex-container",
+      "B. display: flex",
+      "C. container-flex",
+      "D. flex-direction",
     ],
+  },
+  {
+    numb: 14,
+    question: "Which CSS property is used to apply a 3D transformation?",
+    answer: "C. transform",
+    options: ["A. perspective", "B. animation", "C. transform", "D. skew"],
+  },
+  {
+    numb: 15,
+    question: "What does the innerHTML property do in JavaScript?",
+    answer: "A. Gets or sets the content of an HTML element",
+    options: [
+      "A. Gets or sets the content of an HTML element",
+      "B. Adds a new HTML element",
+      "C. Executes JavaScript within an element",
+      "D. Removes an HTML element",
+    ],
+  },
+  {
+    numb: 16,
+    question: "What is a Promise in JavaScript?",
+    answer:
+      "A. An object representing the eventual completion or failure of an async operation",
+    options: [
+      "A. An object representing the eventual completion or failure of an async operation",
+      "B. A method used for error handling",
+      "C. A function that executes synchronously",
+      "D. A loop construct",
+    ],
+  },
+  {
+    numb: 17,
+    question: "What is the output of 1 + '2' in JavaScript?",
+    answer: "A. '12'",
+    options: ["A. '12'", "B. 3", "C. '3'", "D. undefined"],
+  },
+  {
+    numb: 18,
+    question:
+      "Which CSS property is used to specify the area inside the padding and border?",
+    answer: "D. width",
+    options: ["A. padding", "B. content-area", "C. margin", "D. width"],
+  },
+  {
+    numb: 19,
+    question: "What is the default position value in CSS?",
+    answer: "D. static",
+    options: ["A. relative", "B. absolute", "C. fixed", "D. static"],
+  },
+  {
+    numb: 20,
+    question:
+      "Which method in JavaScript is used to bind a function to a specific object?",
+    answer: "C. bind()",
+    options: ["A. call()", "B. apply()", "C. bind()", "D. attach()"],
   },
 ];
